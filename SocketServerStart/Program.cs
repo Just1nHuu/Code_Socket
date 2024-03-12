09bcd9e9556fe4192f1c9e3789e37601ee56d82e
Nguyễn Hữu Tài <138273682+Just1nHuu@users.noreply.github.com>
2024-03-12 17:01:19 +0700

    Gửi dữ liệu từ client  đến server - không ghi dữ liệu

---
+++ SocketServerStart/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServerStart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipaddr = IPAddress.Any;
+
+            IPEndPoint ipep = new IPEndPoint(ipaddr, 23000);
+
+            listenerSocket.Bind(ipep);
+
+            listenerSocket.Listen(5);
+            
+            Console.WriteLine("Accept connected");
+
+            Socket client = listenerSocket.Accept();
+            
+            Console.WriteLine("client connected." + client.ToString() + "-IP End Point: " + client.RemoteEndPoint.ToString());
+
+            byte[] buff = new byte[1024];
+
+            int numberOfReceivedBytes = 0;
+
+            Console.WriteLine("Number of received byte: " + numberOfReceivedBytes);
+
+            Console.WriteLine("Data sent  by client is: " + buff);
+
+            string receivedText = Encoding.ASCII.GetString(buff,0,numberOfReceivedBytes);
+
+            Console.WriteLine("Data sent by client is :", receivedText);
+
+        }
+    }
+}
